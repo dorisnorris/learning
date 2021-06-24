63801c461ae6f180bc7edb1069a0f4547459513f
dorisnorris <norris.doris@outlook.com>
2021-06-24 09:32:28 -0700

    ch3 loops

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,6 +5,16 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'username': 'Doris'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Santa Cruz!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
 	return render_template('index.html', title='Home', user=user)
 
 	
