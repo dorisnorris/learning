f4c9cfd66610ae43f846063006e9f8358458e000
dorisnorris <norris.doris@outlook.com>
2021-06-25 11:27:55 -0700

    added database models

--- microblog/config.py
+++ microblog/config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # this class will store configuration variables
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQALCHEMY_TRACK_MODIFICATIONS = False
